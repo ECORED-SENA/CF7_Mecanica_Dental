fbda74371fe33f6f6228826ce6451975ae29f85a
Luis Perez <ljpm9401@gmail.com>
2022-12-16 20:04:35 -0500

    Ajustes

--- src/views/Tema6.vue
+++ src/views/Tema6.vue
@@ -15,16 +15,47 @@
       .col-md-5(style="place-self:center;")
         figure
           img(src="@/assets/curso/tema6/1.png" data-aos="fade-left")
+    
+    LineaTiempoD.color-secundario.mt-5
+      .row.p-4(numero="1" titulo="Introducción al proceso")
+        figure
+          .video
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/maXNNDyV0Eg?list=PLkc5n6npRWkgYuDj_V5ydcuj1TvZlwfrB" title="Introducción al proceso" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+      .row.p-4(numero="2" titulo="Elaboración de plato base")
+        figure
+          .video
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/UFooUzfiVb0?list=PLkc5n6npRWkgYuDj_V5ydcuj1TvZlwfrB" title="Elaboración de plato base" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+      .row.p-4(numero="3" titulo="Posicionamiento de dientes")
+        figure
+          .video
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/bAj5kF-oLCM?list=PLkc5n6npRWkgYuDj_V5ydcuj1TvZlwfrB" title="Posicionamiento de dientes" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+      .row.p-4(numero="4" titulo="Enmuflado")
+        figure
+          .video
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/YIKo52PW3yU?list=PLkc5n6npRWkgYuDj_V5ydcuj1TvZlwfrB" title="Enmuflado" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+      .row.p-4(numero="5" titulo="Lavado de cera")
+        figure
+          .video
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/NXuZx7IIuts?list=PLkc5n6npRWkgYuDj_V5ydcuj1TvZlwfrB" title="Lavado de cera" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+      .row.p-4(numero="6" titulo="Acrilado")
+        figure
+          .video
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/UwHmReShgh8?list=PLkc5n6npRWkgYuDj_V5ydcuj1TvZlwfrB" title="Acrilado" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+      .row.p-4(numero="7" titulo="Desenmuflado")
+        figure
+          .video
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/PgPX_0rGaV8?list=PLkc5n6npRWkgYuDj_V5ydcuj1TvZlwfrB" title="Desenmuflado" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+      .row.p-4(numero="8" titulo="Pulido")
+        figure
+          .video
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/i9Nq7ceGldI?list=PLkc5n6npRWkgYuDj_V5ydcuj1TvZlwfrB" title="Pulido" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+      .row.p-4(numero="9" titulo="Pulido y brillado")
+        figure
+          .video
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/zLtRddglYoE?list=PLkc5n6npRWkgYuDj_V5ydcuj1TvZlwfrB" title="Pulido y brillado" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+      
 
-    figure.mt-5
-      .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/maXNNDyV0Eg" title="Introducción al proceso" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
-    p.mt-5(data-aos="fade-up") Por otro lado, #[strong la prótesis sobre implantes] se trata de las prótesis dentales de arcada completa que se fijan atornilladas o cementadas sobre seis u ocho implantes osteointegrados. En el siguiente video, se presenta el procedimiento de fabricación:
-
-    figure.mt-5
-      .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/UFooUzfiVb0" title="Elaboración de plato base" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
 
 
